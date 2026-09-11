d4f59730df769e83446ea7df14d834bafbb5f085
Donthireddy Rakshitha <rakshitha232006@gmail.com>
2026-09-11 20:22:21 +0530

    Add camera input

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
 import streamlit as st
 from PIL import Image
-
 from object_detector import detect_objects
 
-
 st.title("Object Detection")
-
-st.write(
-    "Upload an image to detect objects using DETR."
-)
+st.write("Upload an image or take a photo to detect objects using DETR.")
 
 uploaded_file = st.file_uploader(
-    "Select an image",
+    "Upload an image",
     type=["jpg", "jpeg", "png"]
 )
 
+camera_image = st.camera_input("Take a picture")
+
+image = None
 
 if uploaded_file is not None:
-
     image = Image.open(uploaded_file)
 
-    st.subheader("Input Image")
+elif camera_image is not None:
+    image = Image.open(camera_image)
 
-    st.image(
-        image,
-        use_container_width=True
-    )
+if image is not None:
+    st.image(image, caption="Input image")
 
     if st.button("Detect Objects"):
-
         with st.spinner("Detecting objects..."):
-
             processed_image = detect_objects(image)
-
-        st.subheader("Processed Image")
 
         st.image(
             processed_image,
-            use_container_width=True
+            caption="Processed image"
         )
